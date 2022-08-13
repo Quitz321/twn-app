9d7eaad6808f9c835066bce7aed4a6aaa9f0563c
Guido Hansen <Guido.Hansen@telia.ee>
2022-08-13 15:37:30 +0300

    Article ready - will need review when using links in table

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { Component, Suspense } from "react";
+import axios from "axios";
 
 import classes from "./Article.module.css"
 
-const Article = (props) => (
-  <div>
-    <div className={classes.title}>
-      <h1>PEALKIRI</h1>
-    </div>
-    <div className={classes.image}>
-      PILT
-    </div>
-    <div className={classes.content}>
-      SISU
-    </div>
-  </div>
-)
+class Article extends Component {
+
+  state = {
+    id: '972d2b8a',
+    data: null,
+    render: false
+
+  }
+
+  componentDidMount () {
+    const data = axios.get('https://midaiganes.irw.ee/api/list/' + this.state.id).then((res) => {
+      this.setState({data: res.data, render: true})
+    })
+    
+
+  }
+
+  render() {
+    let title, intro, image, body, elements, tags
+    if (this.state.data) {
+     ({title, intro, image, body, tags} = this.state.data)
+    }
+    if (this.state.render) {
+      elements = (
+      <div className={classes.wrapper}>
+        <div className={classes.title}>
+          <h1>{title}</h1>
+        </div>
+        <div>
+          <div className={classes.intro} dangerouslySetInnerHTML={{ __html: intro }}></div>
+        </div>
+        <div className={classes.imageWrapper}>
+          <div className={classes.blur}>
+            <img className={classes.background-image} src={image.large} height="400" width="100%"></img>
+          </div>
+          <img className={classes.image} src={image.small} height="400px" width="600px" alt={image.alt} title={image.title}></img>
+        </div>
+        <div className={classes.body} dangerouslySetInnerHTML={{ __html: body }}>
+        </div>
+        <div className={classes.tagWrapper}>{tags.map((tag) => (
+          <div key={tag} className={classes.tag}>{tag}</div>
+        ))}</div>
+      </div>
+      )
+    } else {elements = (<div>LOADING</div>)}
+
+    return (
+      <div>
+        {elements}
+      </div>
+      
+    )
+  }
+}
 
 export default Article
