eead41029a69f0e4532ae591e16abb4b4bd02c60
Quitz321 <quitz321@gmail.com>
2022-08-14 00:49:22 +0300

    first deploy, responsivness needs a lot of work, table css

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -2,16 +2,26 @@ import React from "react";
 
 const TableRow = (props) => {
 
+  const getBorn = (personal_code) => {
+    const code = String(personal_code)
+    const year = code.substring(1, 3)
+    const month = code.substring(3, 5)
+    const day = code.substring(5, 7)
+
+    const cutoff = (new Date()).getFullYear() - 2000
+
+    return (day + '.' + month + '.' + (year > cutoff ? '19' : '20') + year)
+  }
 
   return (
     <tr>
       <td key="firstname">{props.data.firstname}</td>
       <td key="surname">{props.data.surname}</td>
-      <td key="sex">{props.data.sex}</td>
-      <td key="date">{props.data.date}</td>
+      <td key="sex">{props.data.sex === "m" ? "Mees" : "Naine"}</td>
+      <td key="date">{getBorn(props.data.personal_code)}</td>
       <td key="phone">{props.data.phone}</td>
     </tr>
   )
 }
 
-export default TableRow;
+export default TableRow
